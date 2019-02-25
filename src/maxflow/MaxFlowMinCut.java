775e839f567bb807cc59834d1ac1ec1b160faf74
Zankhana Patel <41522189+Zankhana1995@users.noreply.github.com>
2019-02-25 18:00:27 -0500

    Update MaxFlowMinCut.java
    checkpathExist method commit

--- src/maxflow/MaxFlowMinCut.java
+++ src/maxflow/MaxFlowMinCut.java
@@ -1,5 +1,71 @@
 package maxflow;
 
+import java.util.LinkedList;
+
 public class MaxFlowMinCut {
+	static final int Vertices = 6;
 	
+	
+	//this function will use bfs to check if there is any path exists from source to sink
+	//parent will store the path
+	boolean checkpathExists(int residualGraph[][], int s, int t , int parent[])
+	{
+		boolean visited[] = new boolean[Vertices];
+		for(int i=0;i<Vertices;i++)
+		{
+			visited[i]=false;
+		}
+		LinkedList<Integer> queue = new LinkedList<Integer>(); 
+        queue.add(s); 
+        visited[s] = true; 
+        parent[s]=-1;
+        while(queue.size()!=0)
+        {
+        	int u = queue.poll();
+        	for(int v=0;v<Vertices;v++)
+        	{
+        		if(visited[v]==false && residualGraph[u][v]>0)
+        		{
+        			queue.add(v);
+        			parent[v]=u;
+        			visited[v]=true;
+        		}
+        	}
+        }
+        
+		return (visited[t] == true);
+	}
+	
+	int maxFlow(int graph[][],int s, int t)
+	{
+		int u,v;
+		int maxflow =0;
+		int parent[] = new int[Vertices];
+		int residualGraph[][] = new int[Vertices][Vertices];
+		for( u=0;u<Vertices;u++)
+		{
+			for(v=0;v<Vertices;v++)
+			{
+				residualGraph[u][v] = graph[u][v];
+				
+			}
+		}
+		//make the flow while there is path from source to sink
+		
+		
+		
+		return maxflow;
+		
+	}
+	public static void main(String[] args) {
+		int graph[][] = new int[][] { 
+				{ 0, 16, 13, 0, 0, 0 },
+				{ 0, 0, 10, 12, 0, 0 },
+				{ 0, 4, 0, 0, 14, 0 },			
+				{ 0, 0, 9, 0, 0, 20 },
+				{ 0, 0, 0, 7, 0, 4 },
+				{ 0, 0, 0, 0, 0, 0 } };
+		MaxFlowMinCut m = new MaxFlowMinCut();
+		
+	}
 }
